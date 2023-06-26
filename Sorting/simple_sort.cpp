d514684a6acb74904d10e788fcb4733099cc726c
Leandro Santiago <lsds.snf20@uea.edu.br>
2023-06-26 13:47:57 -0400

    Added simple sort sample

---
+++ Sorting/simple_sort.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+#define MAX 5
+
+using namespace std;
+
+void xxx_sort(int *arr){
+    int min, j, temp;
+
+    for(int i = 0; i < MAX-1; i++){
+        min = i;
+        for(int j = i + 1; j < MAX; j++){
+            if(arr[j] < arr[min]){
+                min = j;
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp; 
+    }
+
+}
+
+int main(){
+    int array[] = {5,2,1,4,7};
+
+    xxx_sort(array);
+
+    for(int i = 0; i < MAX; i++){
+        cout << array[i] << " ";
+    }
+
+    return 0;
+}
