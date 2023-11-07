1d22697ffc2fb4fe661fd466b747aaf6512dda5b
Leandro Santiago <lsds.snf20@uea.edu.br>
2023-11-07 17:23:51 -0400

    Added new bubble sort basic sample

---
+++ Sorting/Bubble Sort/bubble_sort_v2.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void swap(int *arr, int a, int b){
+    int aux;
+    aux = arr[a];
+    arr[a] = arr[b];
+    arr[b] = aux;
+}
+
+void bubble_sort(int *arr, int n){
+    int i, j;
+    bool swaped;
+
+    for(i = 0; i < n-1; i++){
+        swaped = false;
+
+        for(j = 0; j < n-1-i; j++){
+            if(arr[j] > arr[j+1]){
+                swaped = true;
+                swap(arr, j, j+1);
+            }
+        }
+
+        if(!swaped) break;
+
+    }
+
+}
+
+int main(){
+    int arr[] = {5, 3, 1, 2, 4};
+
+    bubble_sort(arr, 5);
+
+    for(int i = 0; i < 5; i++){
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
