fb44874d14df8f93486d1b639741667b46a6105a
Leandro Santiago <lsds.snf20@uea.edu.br>
2023-11-08 17:15:42 -0400

    Update dynamic_allocation.c

--- Stuff/dynamic_allocation.c
+++ Stuff/dynamic_allocation.c
@@ -5,11 +5,13 @@ int main(){
     int x = 2;
     int *y = malloc(100); // Alocando 100 bytes de memória
     int *z = malloc(400); // Alocando 400 bytes de memória
+
     // Stack([z (Heap) | y (Heap) | x = 2])
 
     // int *array = (int *) malloc(5 * sizeof(int)); // Alocando bytes para 5 inteiros na memória 
 
     free(z);
+    
     // Stack([z | y (Heap) | x = 2])
 
     return 0;
